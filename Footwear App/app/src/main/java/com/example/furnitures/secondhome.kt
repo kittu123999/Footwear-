b043c752adcb895feab23318db829059434b8be5
kittu123999 <150127541+kittu123999@users.noreply.github.com>
2023-11-07 14:56:00 +0530

    Add files via upload

---
+++ Footwear App/app/src/main/java/com/example/furnitures/secondhome.kt
@@ -0,0 +1,11 @@
+package com.example.furnitures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secondhome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondhome)
+    }
+}
